59de4a7253086e532dfe342a2a2cb1d8bc2a776a
Fabelt 4∅4 <78570215+Fabelt14@users.noreply.github.com>
2022-10-25 00:53:12 +0100

    Create MAIN.py

---
+++ MAIN.py
@@ -0,0 +1,28 @@
+
+print("ROBOTIC PYTHON \n")
+print("Welcome to this panel \n")
+
+name = input("what is your name? \n")
+
+print("Yo " + name + ", What a nice name, thank me for that by saying Thanks \n")
+
+print("well " + name + " How can we help you \n")
+
+print("we are into: \n")
+
+list = ("Computer Science + Cybersecurity + Physics + Chemistry + and + Mathematics \n")
+
+print(list)
+
+user = input("And " + name + " is into? \n")
+
+
+print(user + " Nice ," + " can we work together? \n")
+
+man2 = input("Yes/No?")
+
+if man2:
+  print("This is my number for contribution 09017545298");
+else:
+    print("Ok Thanks for your time!!!!!!!")
+
